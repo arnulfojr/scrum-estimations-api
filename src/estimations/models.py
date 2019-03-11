b3d87239c2165b59dbc0ee3481ffdec24d35127d
Arnulfo Solis <arnulfojr94@gmail.com>
2019-03-11 01:03:40 +0100

    rename step for value (Sequence value)

--- src/estimations/models.py
+++ src/estimations/models.py
@@ -3,13 +3,13 @@
 Here is a short story of the models:
 
     A Scrum estimation session happens in an organization.
-    Every estimation session has some team members that participate.
+    Each estimation session has some team members that participate.
     In every session a limited set of tasks are estimated by the members.
     The members use a sequence to closely describe the task complexity.
-        Sequences are chained/linked steps,
-        each having an integer value and a name (e.g., shirt-size).
+        Sequences are chained/linked values,
+        each having a numeric value and a name (e.g., shirt-size).
         The numeric value is used to calculate the average of the estimated task,
-        and to keep a history of the estimated task.
+        and to match the closest estimation.
         The name (optionally) can be used to abstract away the numeric value.
 """
 from datetime import datetime
@@ -37,7 +37,7 @@ class Sequence(MixinModel, peewee.Model):
         db_table = 'sequences'
 
 
-class Step(MixinModel, peewee.Model):
+class Value(MixinModel, peewee.Model):
     """Estimation step."""
 
     sequence = peewee.ForeignKeyField(Sequence, related_name='estimation_steps')
@@ -132,7 +132,7 @@ class Estimation(MixinModel, peewee.Model):
 
     user = peewee.ForeignKeyField(User, related_name='estimations')
 
-    value = peewee.ForeignKeyField(Step, related_name='estimations')
+    value = peewee.ForeignKeyField(Value, related_name='estimations')
 
     created_at = peewee.TimestampField(default=datetime.now)
 
@@ -152,7 +152,7 @@ class EstimationSummary(MixinModel, peewee.Model):
 
     task = peewee.ForeignKeyField(Task, related_name='summaries')
 
-    final_value = peewee.ForeignKeyField(Step)
+    closet_value = peewee.ForeignKeyField(Value)
 
     average = peewee.DecimalField()
 
