5718e6321ab69f91f930ddc0dd383230a5016c0f
Arnulfo Solis <arnulfojr94@gmail.com>
2019-05-18 12:52:15 +0200

    users package now only takes json requests on Non-GET methods

--- src/users/app.py
+++ src/users/app.py
@@ -1,8 +1,23 @@
+from aiohttp import web
 from aiohttp.web import Application
 
 from users.routes import router
 
 
-UsersApp = Application()
+@web.middleware
+async def json_middleware(request, handler):
+    if request.method != 'GET' and 'json' not in request.content_type:
+        return web.json_response({
+            'message': 'This is a JSON based API, please use only JSON for requests.',
+        }, status=400)
+
+    return await handler(request)
+
+
+UsersApp = Application(
+    middlewares=[
+        json_middleware,
+    ],
+)
 
 UsersApp.add_routes(router)
